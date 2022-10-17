502f08c220e8a2c9732fc93b910b494412f79248
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2022-10-17 15:06:29 -0700

    desktop social sharing icons javascript show on click added

---
+++ app.js
@@ -0,0 +1,7 @@
+document
+  .querySelector(".social-share-img")
+  .addEventListener("click", () =>
+    document
+      .querySelector(".social-sharing-container")
+      .classList.toggle("show-share-icons")
+  );
